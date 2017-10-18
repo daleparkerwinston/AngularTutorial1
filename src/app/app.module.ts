d4f93b523b95931e1faea8a8d0b34d7a00e5c1e1
Dale Winston <daleparkerwinston@gmail.com>
2017-10-18 17:31:59 -0400

    Finished Basics Tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import {FormsModule} from '@angular/forms';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import {WarningAlertComponent} from './alerts/warning-alert.component';
+import {SuccessAlertComponent} from './alerts/success-alert.component';
+import { UsernameComponent } from './username/username.component';
+import { DirectivesComponent } from './directives/directives.component';
+import {TimeStampComponent} from "./directives/timeStamp.component";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
+    UsernameComponent,
+    DirectivesComponent,
+    TimeStampComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created.';
+  serverStatusTextClass = '';
+  serverName = '';
+  serverCreated = false;
+  servers = ['Test', 'Production', 'Sandbox'];
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = `Server was created! Name is ${this.serverName}.`;
+    this.serverStatusTextClass = 'red';
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+}
